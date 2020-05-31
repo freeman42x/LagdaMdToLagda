2d6bf063d385090cb7581de527f6398de1ef8995
Răzvan Flavius Panda <razvan.panda@gmail.com>
2020-05-31 09:08:12 +0100

    WIP

--- Main.hs
+++ Main.hs
@@ -1,7 +1,29 @@
 module Main where
 
+import Data.Text
+import Data.Text.IO
+import GHC.IO
+import Prelude hiding (readFile, writeFile, putStrLn)
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  fileText <- readFile testFileInput
+  writeFile testFileOutput "42"
 
+-- getAllFilesFromFolderWithExtension ::
 
--- convertFile :: Text -> Text
+testFileInput :: FilePath
+testFileInput = "/home/neo/Forks/plfa.github.io/src/plfa/part1/Naturals.lagda.md"
+
+testFileOutput :: FilePath
+testFileOutput = "/home/neo/Forks/plfa.github.io/src/plfa/part1/Naturals.lagda"
+
+convertFile :: Text -> Text
+convertFile content = "result"
+
+convertText :: Text -> Text
+convertText txt = undefined
+
+testConvert = convertFile "42"
+
+testReadFile = readFile testFileInput
