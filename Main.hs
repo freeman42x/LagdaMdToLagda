1eb37c759e005dabbf22f26ff0de9f9e9718dcc1
Răzvan Flavius Panda <razvan.panda@gmail.com>
2020-05-31 23:15:45 +0100

    Planning

--- Main.hs
+++ Main.hs
@@ -1,21 +1,29 @@
 module Main where
 
+import Control.Monad (return)
 import Data.Text (Text)
 import qualified Data.Text as DT
 import Data.Text.IO (readFile, writeFile, putStrLn)
 import GHC.IO
 import Prelude (concat, zipWith, last, init, repeat, ($), (==))
 
-testFileInput :: FilePath
-testFileInput = "/home/neo/Forks/plfa.github.io/src/plfa/part1/NaturalsRen.lagda.md"
-
-testFileOutput :: FilePath
-testFileOutput = "/home/neo/Forks/plfa.github.io/src/plfa/part1/Naturals.lagda"
+-- testFileInput :: FilePath
+-- testFileInput = "/home/neo/Forks/plfa.github.io/src/plfa/part1/NaturalsRen.lagda.md"
+--
+-- testFileOutput :: FilePath
+-- testFileOutput = "/home/neo/Forks/plfa.github.io/src/plfa/part1/Naturals.lagda"
 
 main :: IO ()
 main = do
-  fileText <- readFile testFileInput
-  writeFile testFileOutput $ convertText fileText
+  -- * find all files with .lagda.md extension under a folder
+  -- * read content of all those files as Text
+  -- and store both read location and content
+  -- * fmap previous to a write location and converted content
+  -- * write previous to respective files
+  return ()
+
+  -- fileText <- readFile testFileInput
+  -- writeFile testFileOutput $ convertText fileText
 
 -- TODO getAllFilesFromFolderWithExtension ::
 
