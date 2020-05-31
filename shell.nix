203a5762a12e7c9a1f2c9ccf26f6f657553c991b
Oren Rozen <countoren@gmail.com>
2020-05-31 17:30:39 -0500

    change shell.nix to use vscodeEnv change nixpkgs param to point to more recent nixpkgs with vscodeEnv

--- shell.nix
+++ shell.nix
@@ -1,35 +1,46 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "default", doBenchmark ? false }:
-
+{ ps ? import <nixpkgs> {}
+, nixpkgs ?  import (ps.fetchFromGitHub {
+    owner = "NixOS";
+    repo = "nixpkgs";
+    rev = "f75d62941d8ad616bfc334ae19d71a0a6677dd19";
+    sha256 = "0n88r4fw1syad9zl7r40r7xlxwx38ni8s9hzyayxssr21ii2p38h";
+}) {}
+, ghcVersion ? "ghc865"
+# did'nt have time to look how to add benchmark but it should not be a big deal
+# , doBenchmark ? false 
+}:
+with nixpkgs;
 let
+  compiler = haskell.packages.${ghcVersion};
+  drv = compiler.callPackage ./project.nix {};
+  vscodeConfig = (callPackag (import ~/Desktop/vscodeEnv {})).vscodeEnv;
+  hie = 
+    ((import (fetchTarball "https://github.com/infinisil/all-hies/tarball/master") {})
+    .selection { selector = p: { ${ghcVersion} = p.${ghcVersion}; }; });
 
-  inherit (nixpkgs) pkgs;
+in 
+drv.env.overrideAttrs (shellEnv: {
+  buildInputs = shellEnv.buildInputs ++ [
+    cabal2nix
+    hie
+    (vscode-utils.vscodeEnv {
+      #usually this path go to tmp but feel free to change it wherever you think it is good place to the "fake" global folder
+      #putting it on empty string will leave the default one.
+      user-data-dir = "./.vscode-globalUserData";
+      #this file will be updated automaticly by vscode when using vscode UI to install/uninstall extensions
+      mutableExtensionsFile = ./mutable-extensions.nix;
+      nixExtensions = [
+        { name = "vscode-hie-server"; publisher = "alanz"; version = "0.0.40"; sha256 = "1cmlgidjma41s5zq5161gcxxmk5lfzcm8dvznls04y5l7q9b0gca";  }
+        { name = "language-haskell"; publisher = "justusadam"; version = "3.2.0"; sha256 = "190h1hky2yy5n00ncqf15mmaizgpm3w9pzvasmi2gangpg4qb6y5";  }
+      ];
 
-  f = { mkDerivation, base, stdenv, text, split, turtle }:
-      mkDerivation {
-        pname = "LagdaMdToLagda";
-        version = "0.1.0.0";
-        src = ./.;
-        isLibrary = false;
-        isExecutable = true;
-        executableHaskellDepends = [ base text split turtle ];
-        license = "unknown";
-        hydraPlatforms = stdenv.lib.platforms.none;
-      };
-  haskellPackages = if compiler == "default"
-                       then pkgs.haskellPackages
-                       else pkgs.haskell.packages.${compiler};
+      # configuration for settings files- those will be overrides/create over the one in .vscode folder
+      # settings = {};
+      # keybindings ={};
+    })
+  ];
 
-  variant = if doBenchmark then pkgs.haskell.lib.doBenchmark else pkgs.lib.id;
-
-  drv = variant (haskellPackages.callPackage f {});
-
-in
-
-  (if pkgs.lib.inNixShell then drv.env else drv).overrideAttrs (_: {
   shellHook = ''
-    PATH="$PATH:${pkgs.ghc}/bin:${pkgs.cabal-install}/bin:${
-      ((import
-        (fetchTarball "https://github.com/infinisil/all-hies/tarball/master")
-        { }).selection { selector = p: { inherit (p) ghc865; }; })
-    }/bin"'';
+    echo 'to start editor with HIE run on the root of the folder: code .'
+  '';
 })
