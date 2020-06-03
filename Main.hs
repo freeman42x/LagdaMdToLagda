d116bc986dab3303ced7e834e5f654bb0de13fb1
Răzvan Flavius Panda <razvan.panda@gmail.com>
2020-06-03 02:34:02 +0100

    Re-format code

--- Main.hs
+++ Main.hs
@@ -1,14 +1,32 @@
 module Main where
 
-import Control.Monad (return)
-import Data.Text (Text)
-import qualified Data.Text as DT
-import Data.Text.IO (readFile, writeFile, putStrLn)
-import GHC.IO
-import Prelude (concat, sequence, zipWith, last, init, repeat, show, (<$>), ($), (.), (==), Bool(..))
-import Prelude as P
-import Turtle (encodeString)
-import Turtle.Prelude (lsif, single, sort)
+import           Control.Monad                  ( return )
+import           Data.Text                      ( Text )
+import qualified Data.Text                     as DT
+import           Data.Text.IO                   ( readFile
+                                                , writeFile
+                                                , putStrLn
+                                                )
+import           GHC.IO
+import           Prelude                        ( concat
+                                                , sequence
+                                                , zipWith
+                                                , last
+                                                , init
+                                                , repeat
+                                                , show
+                                                , (<$>)
+                                                , ($)
+                                                , (.)
+                                                , (==)
+                                                , Bool(..)
+                                                )
+import qualified Prelude                       as P
+import           Turtle                         ( encodeString )
+import           Turtle.Prelude                 ( lsif
+                                                , single
+                                                , sort
+                                                )
 
 -- testFileInput :: FilePath
 -- testFileInput = "/home/neo/Forks/plfa.github.io/src/plfa/part1/NaturalsRen.lagda.md"
@@ -19,8 +37,11 @@ import Turtle.Prelude (lsif, single, sort)
 main :: IO ()
 main = do
   -- * find all files with .lagda.md extension under a folder
-  files <- sort $ lsif (\_ -> return True) "/home/neo/Forks/plfa.github.io/src/plfa/part1/"
-  sequence_ $ P.putStrLn . encodeString <$> files
+  files <- sort
+    $ lsif (\_ -> return True) "/home/neo/Forks/plfa.github.io/src/plfa/part1/"
+  P.sequence_ $ P.putStrLn . encodeString <$> files
+
+  -- rewrite: traverse_ (putStrLn . encodeString) files
 
   -- * read content of all those files as Text
   -- and store both read location and content
@@ -32,13 +53,15 @@ main = do
 --  HACK that does the job
 convertText :: Text -> Text
 convertText txt = conv
-  where
-    splits = DT.splitOn "```\n" txt
-    alternateCodeBlocks = Prelude.concat $ repeat (["\\begin{code}\n", "\\end{code}\n"])
-    conv = convert splits alternateCodeBlocks
+ where
+  splits = DT.splitOn "```\n" txt
+  alternateCodeBlocks =
+    Prelude.concat $ repeat ["\\begin{code}\n", "\\end{code}\n"]
+  conv = convert splits alternateCodeBlocks
 
 convert :: [Text] -> [Text] -> Text
 convert splits alternateCodeBlocks = res
-  where
-    texts = Prelude.concat $ zipWith (\a b -> [a, b]) splits alternateCodeBlocks
-    res = DT.concat $ if last texts == "\\begin{code}\n" then init texts else texts
+ where
+  texts = Prelude.concat $ zipWith (\a b -> [a, b]) splits alternateCodeBlocks
+  res =
+    DT.concat $ if last texts == "\\begin{code}\n" then init texts else texts
